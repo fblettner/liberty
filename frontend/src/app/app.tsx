f6dbf592fe481963ee5c0925d1fd7b34540bb3e1
Franck Blettner <franck.blettner@nomana-it.fr>
2025-02-20 11:12:27 +0100

    Migrate Framework to use liberty-core

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,111 +1,26 @@
 /*
- * Copyright (c) 2022 NOMANA-IT and/or its affiliates.
+ * Copyright (c) 2025 NOMANA-IT and/or its affiliates.
  * All rights reserved. Use is subject to license terms.
  * *
  */
-// React Import
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import * as Sentry from "@sentry/react";
-
-// MUI Import
-import { useMediaQuery } from '@ly_components/common/UseMediaQuery';
-
-// Custom Import
-import { AppDispatch, RootState } from '@ly_app/store';
-import { GlobalSettings } from "@ly_utils/GlobalSettings";
-import { getModules, loadModules, onDarkModeChanged } from '@ly_features/global';
-import { AppsHeader } from '@ly_components/apps/AppsHeader/AppsHeader';
-import { AppsContent } from '@ly_components/apps/AppsContent/AppsContent';
-import { AppsUser } from '@ly_components/apps/AppsUser/AppsUser';
-import { FormsChatbot } from '@ly_components/forms/FormsChatbot/FormsChatbot';
-import { IModulesState } from '@ly_types/lyModules';
-import { Div, Div_AppsLayout } from '@ly_components/styles/Div';
+import { useThemeConfig } from '@ly_styles/theme';
+import { AppsContent, LYThemeProvider, useAppContext } from 'liberty-core';
 
 
 export function App() {
-  // Redux dispatcher
+  const { userProperties, appsProperties, modulesProperties } = useAppContext();
 
-  const dispatch = useDispatch<AppDispatch>()
-  const modulesLoading = useSelector((state: RootState) => state.app.loading)
-  const modules: IModulesState = useSelector(getModules);
+  const { theme } = useThemeConfig({
+    appsProperties,
+    userProperties,
+    modulesProperties,
+  });
 
-  // Declare variables  
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-
-  let isSentryInitialized = false;
-
-  // Use Effect
-  useEffect(() => {
-    setIsLoading(true);
-    const init = async () => {
-
-      dispatch(onDarkModeChanged(prefersDarkMode));
-
-      switch (modulesLoading) {
-        case "idle":
-          await dispatch(loadModules());
-          break;
-        case "succeeded":
-          setIsLoading(false);
-          if (!isSentryInitialized && modules.sentry.enabled && modules.sentry.params) {
-            // Parse the JSON string
-            const parsedData = JSON.parse(modules.sentry.params);
-            const url = parsedData.url;
-            const replay = parsedData.replay === "true"; 
-
-            Sentry.init({
-              dsn: url,
-              integrations: [
-                Sentry.browserTracingIntegration(),
-                Sentry.replayIntegration({
-                  maskAllText: false,
-                  blockAllMedia: false,
-                  unblock: [".sentry-unblock, [data-sentry-unblock]"],
-                  unmask: [".sentry-unmask, [data-sentry-unmask]"],
-                }),
-              ],
-              beforeBreadcrumb(breadcrumb, hint) {
-                // Filter out XHR breadcrumbs
-                if (breadcrumb.category === "xhr") {
-                  return null; // Discard the XHR breadcrumb
-                }
-                return breadcrumb; // Keep other breadcrumbs
-              },
-              // Tracing
-              tracesSampleRate: replay ? 1.0 : 0.0, //  Capture 100% of the transactions
-              tracePropagationTargets: ["localhost", /^https:\/\/nomana-it.sentry\.io\/api/],
-              // Session Replay
-              replaysSessionSampleRate: 0.0,
-              replaysOnErrorSampleRate: 1.0,
-              normalizeDepth: 10,
-
-            });
-            isSentryInitialized = true;
-          }
-          break;
-      }
-
-    }
-    init();
-
-  }, [dispatch, modulesLoading]);
-
-  if (isLoading) {
-    return (
-      <Div>
-        {GlobalSettings.loading}
-      </Div>
-    )
-  }
-  else
-    return (
-      <Div_AppsLayout>
-        <AppsHeader />
+  return (
+    <LYThemeProvider customTheme={theme}>
         <AppsContent />
-        <AppsUser />
-        <FormsChatbot />
-      </Div_AppsLayout>
-    );
+    </LYThemeProvider>
+
+  );
 }
+
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,13 @@
 /*
- * Copyright (c) 2022 NOMANA-IT and/or its affiliates.
+ * Copyright (c) 2025 NOMANA-IT and/or its affiliates.
  * All rights reserved. Use is subject to license terms.
  * *
  */
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { Provider } from "react-redux"
-import { store } from "@ly_app/store"
 import { App } from "@ly_app/app"
-import "@ly_translations/i18n";
-import { DndProvider } from "react-dnd"
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { LYThemeProvider } from "@ly_styles/theme"
-import { AuthProvider } from "react-oidc-context";
-import { ErrorBoundary } from '@sentry/react';
-import { Div } from '@ly_components/styles/Div';
-import { ClickProvider } from "@ly_components/context/ClickContext"
-import { ZIndexProvider } from "@ly_components/context/ZIndex"
+import { AppProvider, Div, ErrorBoundary, TranslationProvider } from "liberty-core"
+import { AuthProvider, useAuth as oidcUseAuth } from "react-oidc-context";
 
 const oidcConfig = {
   authority: window.location.origin + "/oidc/realms/Liberty",
@@ -24,23 +15,16 @@ const oidcConfig = {
   redirect_uri: window.location.origin,
 };
 
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider {...oidcConfig}>
-      <Provider store={store}>
-        <LYThemeProvider>
-          <DndProvider backend={HTML5Backend}>
-            <ErrorBoundary fallback={<Div>An error has occurred</Div>}>
-              <ClickProvider>
-              <ZIndexProvider>
-                <App />
-                </ZIndexProvider>
-              </ClickProvider>
-            </ErrorBoundary>
-          </DndProvider>
-        </LYThemeProvider>
-      </Provider>
+      <AppProvider useAuth={oidcUseAuth}>
+        <ErrorBoundary fallback={<Div>An error has occurred</Div>}>
+          <TranslationProvider>
+            <App />
+          </TranslationProvider>
+        </ErrorBoundary>
+      </AppProvider>
     </AuthProvider>
   </React.StrictMode>,
 
