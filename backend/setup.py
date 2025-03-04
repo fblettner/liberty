2006eb1bde0d4d02dc2f173df9006be7009b9f0e
Franck Blettner <franck.blettner@nomana-it.fr>
2025-03-04 20:59:22 +0100

    Add a delay before building the docker image

--- backend/setup.py
+++ backend/setup.py
@@ -17,7 +17,7 @@ def read_readme():
 
 setup(
     name="liberty-framework",
-    version="6.0.50",
+    version="6.0.51",
     description="Liberty Framework",
     author="Franck Blettner",
     author_email="franck.blettner@nomana-it.fr",
